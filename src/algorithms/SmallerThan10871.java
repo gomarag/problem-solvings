0bec37d40a3b51c5f90195b26abec1fa545648a2
irriver <maraqann@gmail.com>
2020-12-01 23:30:42 +0900

    배열을 저장하지 않고 바로 출력, 동시에 compareTo로 문자열 비교하려 단, "10".compareTo("5")의 경우에는 10에서 첫 글자인 1의 아스키코드 변환값과5를 비교해서 -4가 나오게 된다. 10을 5보다 작게 판단하는 것. 그래서 int로 바꿔줘야 할 듯

--- src/algorithms/SmallerThan10871.java
+++ src/algorithms/SmallerThan10871.java
@@ -12,22 +12,17 @@ public class SmallerThan10871 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		String[] N_X = br.readLine().trim().split("\\s"); //N_X
-		String[] elems = br.readLine().trim().split("\\s");	//array_A
-		
-		int[] data = new int[Integer.parseInt(N_X[0])];
-		int pivot = Integer.parseInt(N_X[1]);
+		String[] data = br.readLine().trim().split("\\s");	//array_A
 		
 		for (int i = 0; i < data.length; i++) {
-			data[i] = Integer.parseInt(elems[i]);
-		}
-		
-		for (int i = 0; i < data.length; i++) {
-			if (data[i] < pivot) {
-				bw.write(data[i] + "\s");
+			if (data[i].compareTo(N_X[1]) < 0) {
+				bw.write(data[i] + " ");
 			}
 		}
 		br.close();
 		bw.close();
+		
+//		System.out.println("10".compareTo("5"));	//-4
+//		System.out.println("b".compareTo("a"));		//1
 	}
-
 }
