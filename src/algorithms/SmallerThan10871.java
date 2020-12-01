a0fd115c3a410cc62a44f41e805b65241e73e10c
irriver <maraqann@gmail.com>
2020-12-01 22:59:41 +0900

    문제 번호 수정

--- src/algorithms/SmallerThan10871.java
+++ src/algorithms/SmallerThan10871.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class SmallerThan1087 {
+public class SmallerThan10871 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
