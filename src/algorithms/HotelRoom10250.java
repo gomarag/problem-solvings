93cc4ef79dc9c55c780e366fb447807c01bbe9b5
irriver <maraqann@gmail.com>
2020-12-09 19:36:13 +0900

    데이터 입력 완료: 토큰을 2차원 배열로

--- src/algorithms/HotelRoom10250.java
+++ src/algorithms/HotelRoom10250.java
@@ -10,14 +10,16 @@ public class HotelRoom10250 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(buffer.readLine().trim()); //2
-		String[][] testcase = new String[T][3];
+		int T = Integer.parseInt(buffer.readLine()); //2
+		
+/*		2
+		6 12 10
+		30 50 72
+*/
+		String[][] testCase = new String[T][3];
 		
 		for (int i = 0; i < T; i++) {
-			for (int j = 0; j < 3; j++) {
-				testcase[i] = buffer.readLine().split("\\s"); //testcase[0] = 6, 12, 10
-			}
+			testCase[i] = buffer.readLine().split("\\s"); //6, 12, 10 | 30, 50, 72
 		}
-		System.out.println(testcase[0][1]);
 	}
 }
