ef9d1259769016e575387cfa11c6031628c17a51
irriver <maraqann@gmail.com>
2020-12-09 21:40:58 +0900

    로직 완성: XXYY, XYY로 표기법 수정 필요

--- src/algorithms/HotelRoom10250.java
+++ src/algorithms/HotelRoom10250.java
@@ -10,7 +10,7 @@ public class HotelRoom10250 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(buffer.readLine()); //2
+		int T = Integer.parseInt(buffer.readLine().trim()); //2
 		
 /*		2
 		6 12 10
@@ -20,6 +20,17 @@ public class HotelRoom10250 {
 		
 		for (int i = 0; i < T; i++) {
 			testCase[i] = buffer.readLine().split("\\s"); //6, 12, 10 | 30, 50, 72
+			int H = Integer.parseInt(testCase[i ][0]);
+			int W = Integer.parseInt(testCase[i][1]);
+			int N = Integer.parseInt(testCase[i][2]);
+			
+			String space = (N / H) < 10 ? "0" : "";
+			
+			if (N % H == 0) {
+				System.out.println(H + space + (N / H));
+			} else {
+				System.out.println((N % H) + space + (N / H + 1));
+			}
 		}
 	}
 }
