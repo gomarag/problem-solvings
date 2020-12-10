640bf23b9c7f100b635f078e089d7e7b816144ae
irriver <maraqann@gmail.com>
2020-12-10 22:04:40 +0900

    숫자 범위 주의: 결과 long으로 변환 후 통과, 스캐너 사용

--- src/algorithms/Superviser13458.java
+++ src/algorithms/Superviser13458.java
@@ -10,10 +10,10 @@ public class Superviser13458 {
 		F:7 S:2
 		
 		1) first 만으로 충분한 경우
-			An - F <= 0
+			수험생 수 - 총 감독 감시 가능 인원 <= 0
 		2) first + second 까지 필요한 경우
-			first = N
-			second = 올림(An / second)
+			총 감독 수 = 고사장 수
+			부 감독 수 = 올림((수험생 수 - 총 감독 감시 가능 인원)/ 부 감독 감시 인원)
 */
 		Scanner scan = new Scanner(System.in);
 		int N = scan.nextInt();
@@ -26,16 +26,18 @@ public class Superviser13458 {
 		long firstCoverage = scan.nextLong();
 		double secondCoverage = scan.nextDouble();
 		
-		int superviser = 0; double vice = 0;
+		long superviser = 0; double vice = 0;
 		
 		for (int i = 0; i < examinee.length; i++) {
+			//총 감독만으로 감시 가능함: 어쨌든 고사장마다 총 감독은 1명이 필요
 			if (examinee[i] <= firstCoverage) {
 				superviser++;
+			//총 감독 외에 부감독이 필요함: examinee[i] > firstCoverage 
 			} else {
 				superviser++;
 				vice += Math.ceil((examinee[i] - firstCoverage) / secondCoverage);
 			}
 		}
-		System.out.println(superviser + (int)vice);
+		System.out.println(superviser + (long)vice);
 	}
 }
