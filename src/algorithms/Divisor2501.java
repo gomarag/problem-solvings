1ec1f440f1308236b8f8291af677990048f69516
irriver <maraqann@gmail.com>
2020-12-11 22:53:57 +0900

    문제 접근 방법만 정리함

---
+++ src/algorithms/Divisor2501.java
@@ -0,0 +1,39 @@
+/*
+ * - 약수를 모두 구한 후에 0번 인덱스부터 카운팅 해가는 방법
+ * - 약수 구하지 않고 제곱수/안제곱수 확인하고 '약수의 개수'만 구할 것
+ * 
+ * 약수의 개수?
+ * 1) 제곱수면 제곱근 기준으로 대칭: 홀수
+ * 2) 안제곱수면 대칭: 짝수
+ * 
+ * 제곱수임을 확인하려면? 
+ * */
+
+package algorithms;
+
+import java.util.Scanner;
+
+public class Divisor2501 {
+	static boolean isSquareNum() {
+		
+		return true;
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int p = scan.nextInt();
+		int q = scan.nextInt();
+		
+		int divisor = 0;
+		int N = 2;
+		
+		while (true) {
+			if (p % N == 0) {	/* 나눠 떨어지면 약수 */
+				divisor++;
+			} else {			/* 안 나눠 떨어지면 다음으로 */
+				N++;
+			}
+		}
+	}
+
+}
