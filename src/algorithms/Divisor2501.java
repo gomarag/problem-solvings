8235acdc09ff8cd6f473b76c0865ad5edf4ba527
irriver <maraqann@gmail.com>
2020-12-13 21:11:00 +0900

    완성:약수 저장 없이 완전탐색

--- src/algorithms/Divisor2501.java
+++ src/algorithms/Divisor2501.java
@@ -1,12 +1,9 @@
 /*
- * - 약수를 모두 구한 후에 0번 인덱스부터 카운팅 해가는 방법
- * - 약수 구하지 않고 제곱수/안제곱수 확인하고 '약수의 개수'만 구할 것
+ * 완전탐색을 사용할 생각.
+ * 약수를 하나씩 구해가면서 약수를 찾을 때마다 k를 차감해간다.
+ * k = 0이 되면 그 순간의 약수를 반환.
+ * 단, 약수가 자연수 N만큼 커졌는데도 k가 여전히 양수라면 약수 개수가 k보다 작은 것이므로 0 반환.
  * 
- * 약수의 개수?
- * 1) 제곱수면 제곱근 기준으로 대칭: 홀수
- * 2) 안제곱수면 대칭: 짝수
- * 
- * 제곱수임을 확인하려면? 
  * */
 
 package algorithms;
@@ -14,53 +11,21 @@ package algorithms;
 import java.util.Scanner;
 
 public class Divisor2501 {
-	static boolean isSquareNum(int nb) {
-		if (nb <= 0) {
-			return false;
-		}
-		
-		int i = 1;
-		while (i < (nb / i)) {
-			i++;
-		}
-		boolean result = Math.sqrt(nb) == i ? true : false;
-		return result;
-	}
-
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int p = scan.nextInt();
-		int q = scan.nextInt();
-		int[] divisor = new int[p / 2 + 1];
+		int N = scan.nextInt();
+		int k = scan.nextInt();
 		
-		int count = 0;	/* 제곱근까지의 약수 갯수*/
-		int i = 1;
-		int idx = 0;
-		while (i < Math.sqrt(p)) {
-			if (p % i == 0) {
-				divisor[idx++] = i;
-				count++;
-				i++;
-			} else i++;
-		}
-		/* 전체 약수 개수 == divisor.len */
-		if (isSquareNum(p)) {
-			count = count * 2 + 1;
-			divisor[idx] = (int) Math.sqrt(p);
-			for (int o = idx + 1; o < count; o++) {
-				divisor[o] = p / divisor[count - o - 1];
-			}
-		} else {
-			count *= 2;
-			for (int o = idx; o < count; o++) {
-				divisor[o] = p /divisor[count - o - 1];
+		for (int divisor = 1; divisor <= N; divisor++) {
+			if (N % divisor == 0) {
+				k--;
+				if (k == 0) {
+					System.out.println(divisor);
+					return;
+				}
 			}
 		}
-		
-		if (q > count) {
-			System.out.println(0);
-		}
-		
-		System.out.println(divisor[q - 1]);
+		System.out.println(0);
 	}
 }
