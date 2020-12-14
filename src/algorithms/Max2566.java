16d76d5569f6afdcc8da35946635391a29aa3a35
irriver <maraqann@gmail.com>
2020-12-14 20:57:06 +0900

    기존 문자열에서 위치를 찾아야 함, 완전탐색?

--- src/algorithms/Max2566.java
+++ src/algorithms/Max2566.java
@@ -16,14 +16,20 @@ public class Max2566 {
 			sb.append(" ");
 		}
 		buffer.close();
-		
-		String[] token = new String[81];
+		//문자열이 있음: line
+		//"1 2 3 4 5"에서 3의 인덱스를 알고 싶음
+		String[] line = new String[81];
 		int[] data = new int[81];
-		for (int i = 0; i <81; i++) {
-			token = sb.toString().split("\\s");
-			data[i] = Integer.parseInt(token[i]);
+		for (int i = 0; i < 81; i++) {
+			line = sb.toString().split("\\s");
+			data[i] = Integer.parseInt(line[i]);
 		}
 		Arrays.sort(data);
 		System.out.println(data[80]);
+		
+		char ch = (char) data[80];
+		System.out.println(ch);
+		System.out.println((int)ch);
+		System.out.println(sb.toString().indexOf((int)ch));
 	}
 }
