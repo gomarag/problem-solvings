d34aea121f6ee948ddbac59b655c7ae385c4ed38
irriver <maraqann@gmail.com>
2020-12-15 16:31:40 +0900

    일치하는 문자 배열의 인덱스 반환하도록

--- src/algorithms/Max2566.java
+++ src/algorithms/Max2566.java
@@ -17,19 +17,26 @@ public class Max2566 {
 		}
 		buffer.close();
 		//문자열이 있음: line
-		//"1 2 3 4 5"에서 3의 인덱스를 알고 싶음
-		String[] line = new String[81];
+		//"1 2 3 4 5"에서 3의 인덱스
 		int[] data = new int[81];
+		String[] line = new String[8];
 		for (int i = 0; i < 81; i++) {
 			line = sb.toString().split("\\s");
 			data[i] = Integer.parseInt(line[i]);
 		}
 		Arrays.sort(data);
-		System.out.println(data[80]);
+		System.out.println("Max: " + data[80]);
 		
-		char ch = (char) data[80];
-		System.out.println(ch);
-		System.out.println((int)ch);
-		System.out.println(sb.toString().indexOf((int)ch));
+		String max = data[80] + "";
+		System.out.println("maxStr: " + max);
+		int maxIdx = 0;
+		for (int i = 0; i < line.length; i++) {
+			if (line[i] == max) {
+				maxIdx = i;
+				return;
+			}
+		}
+		
+		System.out.println("maxIdx: " + maxIdx);
 	}
 }
