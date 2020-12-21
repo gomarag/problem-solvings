70fe2b4a721cd56e44ac22b031e5eddc437796d8
irriver <maraqann@gmail.com>
2020-12-21 21:52:13 +0900

    사탕 나누기: 임의의 수 k에 관해 정리

---
+++ src/algorithms/Candies14568.java
@@ -0,0 +1,25 @@
+package algorithms;
+
+public class Candies14568 {
+
+	public static void main(String[] args) {
+/*
+        T택희 = 2k(2개 부터)
+        N남규 = Y영훈 + 2(3개 부터)
+        
+        t = 2k
+        n = 2 + k
+        y = k
+		candies = 4k + 2 = 2(2k +1)일 때만 해를 가짐
+		
+        [영훈까지 주고 남은 사탕]이 [영훈이 받아야 할 사탕보다 작거나 같다면] 종료
+        
+        candies = 6
+        t	n	y
+        ----------
+        2k	2+k	k
+        2	3	1 *
+
+*/
+	}
+}
